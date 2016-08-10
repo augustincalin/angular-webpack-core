a25b8048f3752fd61c7d05012364c60196b178ab
Gusti <augustincalin@yahoo.com>
2016-08-10 22:21:53 +0200

    more work

--- Code/WebAppSolution/src/WebApp/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ Code/WebAppSolution/src/WebApp/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -6,13 +6,15 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
+using WebApp.Data;
 
-namespace WebApp.Data.Migrations
+namespace WebApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("00000000000000_CreateIdentitySchema")]
+    partial class CreateIdentitySchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.0-rc3")
---
+++ Code/WebAppSolution/src/WebApp/src/main.js
@@ -0,0 +1,7 @@
+"use strict";
+require('core-js');
+require('reflect-metadata');
+require('zone.js/dist/zone');
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var app_component_ts_1 = require('./app/app.component.ts');
+platform_browser_dynamic_1.bootstrap(app_component_ts_1.AppComponent);
