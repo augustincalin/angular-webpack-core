3f870221e5164a3c6bdf017a86d292ed0a7be08e
Gusti <augustincalin@yahoo.com>
2016-08-13 17:52:14 +0200

    adds xUnit and Moq

---
+++ Code/WebAppSolution/test/WebAppTest/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Controllers;
+using Xunit;
+
+namespace WebAppTest
+{
+    public class Class1
+    {
+        public object ValueController { get; private set; }
+
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, 4);
+        }
+        [Fact]
+        public void TestController()
+        {
+            ValueController controller = new ValueController();
+            string result = controller.Index();
+            string expected = "123";
+            Assert.Equal(result, expected);
+        }
+
+    }
+}
